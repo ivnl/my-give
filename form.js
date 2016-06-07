45e1fac21cbbd0764a9a3d096e042d437e0b0ca5
Moshe Farber <moshefarber1@gmail.com>
2016-06-07 00:40:44 -0400

    added animals page

--- form.js
+++ form.js
@@ -6,13 +6,13 @@ function validateForm() {
     if (category == "cancer") window.open("susangkomen.html", '_self', 'resizable,location,menubar,toolbar,scrollbars,status');
     else if (category == "health") window.open("susangkomen.html", '_self', 'resizable,location,menubar,toolbar,scrollbars,status');
     else if (category == "education") window.open("boysgirlsclub.html", '_self', 'resizable,location,menubar,toolbar,scrollbars,status');
-    else if (category == "animals") window.open("cancer.html", '_self', 'resizable,location,menubar,toolbar,scrollbars,status');
+    else if (category == "animals") window.open("thehumanesociety.html", '_self', 'resizable,location,menubar,toolbar,scrollbars,status');
     else if (category == "cultural") window.open("boysgirlsclub.html", '_self', 'resizable,location,menubar,toolbar,scrollbars,status');
     else if (category == "community") window.open("boysgirlsclub.html", '_self', 'resizable,location,menubar,toolbar,scrollbars,status');
     else if (category == "space") window.open("boysgirlsclub.html", '_self', 'resizable,location,menubar,toolbar,scrollbars,status');
     else if (category == "science") window.open("boysgirlsclub.html", '_self', 'resizable,location,menubar,toolbar,scrollbars,status');
     else if (category == "catastrophe" || category == "natural disaster") window.open("cancer.html", '_self', 'resizable,location,menubar,toolbar,scrollbars,status');
-    else if (category == "environmental") window.open("cancer.html", '_self', 'resizable,location,menubar,toolbar,scrollbars,status');
+    else if (category == "environmental") window.open("thehumanesociety.html", '_self', 'resizable,location,menubar,toolbar,scrollbars,status');
     else if (category == "religion") window.open("cancer.html", '_self', 'resizable,location,menubar,toolbar,scrollbars,status');
     else if (category == "university") window.open("boysgirlsclub.html", '_self', 'resizable,location,menubar,toolbar,scrollbars,status');
     return false;
